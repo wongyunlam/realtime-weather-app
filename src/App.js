8ceffd9e8c48cb4248374d2dfa4ab370ac847564
YL WONG <jay@YLs-MacBook-Pro.local>
2021-07-23 19:08:19 +0800

    web version Weather App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@emotion/react'
+import React, { useEffect, useMemo, useState } from 'react'
+import { getMoment, findLocation } from './utils/helpers'
+import WeatherCard from './views/WeatherCard'
+import styled from '@emotion/styled'
+import useWeatherAPI from './hooks/useWeatherAPI'
+import WeatherSetting from './views/WeatherSetting'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
 }
 
-export default App;
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.backgroundColor};
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const AUTHORIZATION_KEY = 'CWB-507B37E0-0383-4D8C-878D-628B54EC3536'
+
+const App = () => {
+  const storageCity = localStorage.getItem('cityName') || '臺北市'
+  const [currentPage, setCurrentPage] = useState('WeatherCard')
+  const [currentCity, setCurrentCity] = useState(storageCity)
+  const currentLocation = useMemo(
+    () => findLocation(currentCity),
+    [currentCity]
+  )
+  const { cityName, locationName, sunriseCityName } = currentLocation
+  const handleCurrentPageChange = (currentPage) => {
+    setCurrentPage(currentPage)
+  }
+
+  const handleCityChange = (currentCity) => {
+    setCurrentCity(currentCity)
+  }
+
+  const [currentTheme, setCurrentTheme] = useState('light')
+  const [weatherElement, fetchData] = useWeatherAPI({
+    locationName,
+    cityName,
+    authorizationKey: AUTHORIZATION_KEY,
+  })
+
+  // TODO: 等使用者可以修改地區時要修改裡面的參數
+  const moment = useMemo(() => getMoment(sunriseCityName), [sunriseCityName])
+
+  useEffect(() => {
+    setCurrentTheme(moment === 'day' ? 'light' : 'dark')
+  }, [moment])
+
+  return (
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        {currentPage === 'WeatherCard' && (
+          <WeatherCard
+            cityName={cityName}
+            weatherElement={weatherElement}
+            moment={moment}
+            fetchData={fetchData}
+            handleCurrentPageChange={handleCurrentPageChange}
+          />
+        )}
+        {currentPage === 'WeatherSetting' && (
+          <WeatherSetting
+            cityName={cityName}
+            handleCurrentPageChange={handleCurrentPageChange}
+            handleCityChange={handleCityChange}
+          />
+        )}
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
